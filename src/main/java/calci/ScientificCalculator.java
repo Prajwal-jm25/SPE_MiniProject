843b5423ffbfd7f1c631fbbe88a778f8b51b8ccc
Prajwal-jm25 <prajwal.jm25@gmail.com>
2023-03-16 10:14:38 +0530

    Logger fixes

--- src/main/java/calci/ScientificCalculator.java
+++ src/main/java/calci/ScientificCalculator.java
@@ -2,8 +2,8 @@ package calci;
 
 import java.lang.Math;
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class ScientificCalculator {
 
