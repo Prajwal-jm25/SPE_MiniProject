d9b63b53185fa116b16737d3d40b73e57c61f4ee
Prajwal-jm25 <prajwal.jm25@gmail.com>
2023-03-20 01:02:38 +0530

    Updated log statements in the program

--- src/main/java/calci/ScientificCalculator.java
+++ src/main/java/calci/ScientificCalculator.java
@@ -31,15 +31,12 @@ public class ScientificCalculator {
 
     public static void main(String[] args) {
 
-        logger.debug("hello from log4j2");
-        logger.debug("Hi");
-        logger.debug("Running Calculator");
-        logger.debug("Choose");
-        logger.debug("Bye");
+        logger.debug("Starting the program");
         Scanner scanner = new Scanner(System.in);
         int c = 0;
 
         while(c != 5){
+            logger.debug("User Input");
             System.out.println("\nWelcome to ScientificCalculator\nPlease Select the operation which you want to perform\n");
             System.out.println("1) Square Root");
             System.out.println("2) Factorial");
@@ -50,31 +47,41 @@ public class ScientificCalculator {
 
             switch(c){
                 case 1:
+                    logger.debug("Input for Square Root Choice");
                     System.out.println("Enter the Number");
                     double root = scanner.nextDouble();
+                    logger.debug("Executing square root function");
                     System.out.println("The Square Root of " + root + " : " + sqrt(root));
                     break;
                 case 2:
+                    logger.debug("Input for Factorial of a Number");
                     System.out.println("Enter the Number");
                     int num = scanner.nextInt();
+                    logger.debug("Executing factorial function");
                     System.out.println("The Factorial of " + num + " : " + fact(num));
                     break;
                 case 3:
+                    logger.debug("Input for Natural Log of a Number");
                     System.out.println("Enter the Number");
                     double log = scanner.nextDouble();
+                    logger.debug("Executing Natual Log function");
                     System.out.println("The Natural Log of " + log + " : " + log(log));
                     break;
                 case 4:
+                    logger.debug("Input for Power function");
                     System.out.println("Enter the Base");
                     double b = scanner.nextDouble();
                     System.out.println("Enter the exponent");
                     double e = scanner.nextDouble();
+                    logger.debug("Executing power function");
                     System.out.println(b + "to the power of " + e + " : " + pow(b, e));
                     break;
                 case 5:
+                    logger.debug("Exit");
                     System.out.println("GoodBye!");
                     break;
                 default:
+                    logger.debug("Invalid Choice");
                     System.out.println("Invalid Choice!");
                     break;
             }
